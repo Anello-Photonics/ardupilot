0f2a077899b5d4ac32d6ad0391bbf8df89205ff4
Gordon Keller <gordon.keller@anellophotonics.com>
2025-02-13 16:50:16 -0800

    Updated the ExternalAHRS systems to include Anello X3

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS.h
@@ -62,6 +62,9 @@ public:
         // 8 reserved for SBG
         // 9 reserved for EulerNav
         // 10 reserved for Aeron
+#if AP_EXTERNAL_AHRS_MICROSTRAIN7_ENABLED
+         AnelloX3 = 11,
+#endif
     };
 
     static AP_ExternalAHRS *get_singleton(void) {
